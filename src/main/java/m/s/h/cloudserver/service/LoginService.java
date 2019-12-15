8132b257385c86822c1a8a2e5f532544cdf4ecc5
문상혁 <msk2292@naver.com>
2019-12-16 00:06:15 +0900

    다음 TODO 추가

--- src/main/java/m/s/h/cloudserver/service/LoginService.java
+++ src/main/java/m/s/h/cloudserver/service/LoginService.java
@@ -29,11 +29,14 @@ public class LoginService {
     LoginHistoryRepository loginHistoryRepository;
 
     public Result join(String email, String nick_name, String password){
+        //TODO Validaiton Check
+
         Members members = new Members();
         passwordEncoder.encode(password);
         members.setPassword(password);
         members.setEmail(email);
         members.setNick_name(nick_name);
+
         return new Result(200, memberRepository.save(members), null);
     }
 
