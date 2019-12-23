b790a94d980941c13efe82ef462b43498fab8416
문상혁 <msk2292@naver.com>
2019-12-23 22:56:49 +0900

    Spring Security Zuul 로 이관작업중

--- src/main/java/m/s/h/cloudserver/config/SwaggerConfig.java
+++ src/main/java/m/s/h/cloudserver/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package m.s.h.cloudserver.config;
 
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
