4b14e8f696fe6037a86813a06666e52d45d9329c
문상혁 <msk2292@naver.com>
2019-12-31 14:44:26 +0900

    git push test

--- src/main/java/m/s/h/cloudserver/CloudserverApplication.java
+++ src/main/java/m/s/h/cloudserver/CloudserverApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class CloudserverApplication {
+    //server change and git wehbook test
     public static void main(String[] args) {
         SpringApplication.run(CloudserverApplication.class, args);
     }
